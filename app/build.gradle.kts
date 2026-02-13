78d65d1d597d746a7f8286ec8838c1ef61471d7d
Zuyeonn <162269448+Zuyeonn@users.noreply.github.com>
2026-02-13 13:32:51 +0900

    fix: sdk 버전 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.ad_app"
-        minSdk = 26
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
